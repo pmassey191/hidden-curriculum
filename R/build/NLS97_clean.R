4ae2442ba2adc71ca06fe36d24caa80ee454809b
pmassey191 <pmassey@utexas.edu>
2022-02-05 18:03:20 -0600

    Cleans NLS

--- R/build/NLS97_clean.R
+++ R/build/NLS97_clean.R
@@ -1,1 +1,28 @@
-read.csv()
+library(tidyverse)
+library(tidyverse)
+library(here)
+
+NLS97 <- read_csv(here("Data/NLS97_raw.csv"))
+
+head(NLS97)
+
+NLS97 <- NLS97 %>% 
+  mutate(across(starts_with("E"), ~case_when(
+  .x < 0 ~ NA_real_,
+  .x == 99 ~ NA_real_,
+  TRUE ~ .x
+  ))) %>% 
+  filter(if_any(starts_with("E"), ~!is.na(.x))) %>% 
+  rowwise() %>% 
+  mutate(total_arrests = sum(c_across(starts_with("E")), na.rm = TRUE)) %>% 
+  ungroup() %>% 
+  mutate(gender = if_else(R0536300 == 1, "Male", "Female")) %>% 
+  mutate(race = case_when(
+    R1482600 == 1 ~ "Black",
+    R1482600 == 2 ~ "Hispanic",
+    R1482600 == 3 ~ "Mixed Race (Non-Hispanic)",
+    R1482600 == 4 ~ "Non-Black / Non-Hispanic",
+  )) %>% 
+  select(race, gender, total_arrests)
+  
+write_csv(NLS97,"Data/NLS97_clean.csv")
